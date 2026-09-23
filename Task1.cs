8b8e6aed203838bb87c99d30652eb84cc79d95fc
Oziichuk Yaroslav <oziichuk.i.i_ipz24@rcit.ukr.education>
2026-09-23 18:55:41 +0300

    Завдання 1

---
+++ Task1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Task1
+{
+    public static void Task()
+    {
+        List<string> log = new List<string>
+        {
+            "Запис 1: Система запущена",
+            "Запис 2: Користувач увійшов",
+            "Запис 3: Помилка з'єднання",
+            "Запис 4: Спроба повторного підключення",
+            "Запис 5: Успішно підключено"
+        };
+
+        Console.WriteLine("Журнал у зворотному порядку:");
+        
+        for (int i = log.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(log[i]);
+        }
+    }
+}
